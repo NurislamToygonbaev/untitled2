6bca1721771e10599d6a132444bf1442f130bd9c
Nurislam Toygonbaev <ntoygonbaev098@gmail.com>
2023-12-29 19:44:58 +0600

    fghjsd

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("NUke");
-        sdfdsfkfsdlf
-            fsd'fk'sdf
-            fsd'fsdf
-            fsdfsdfsdf
-            s
+        System.out.println("NUke");
     }
 }
